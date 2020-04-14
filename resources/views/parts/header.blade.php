184d0b5d273ccf547c83ada607f9c860c8d9e9e3
gugun_guntur <gugunguntur99@gmail.com>
2020-04-14 10:50:43 +0700

    change header asisten

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -9,7 +9,7 @@
                 <li class="nav-item">
                     <a class="nav-link  " id="search_id" href="#">
                         <i class="mdi mdi-google-assistant mdi-24px align-middle "></i>
-                        Butuh Bantuan? Klik Disini!
+                        Butuh Bantuan?
                     </a>
                 </li>
             </ul>
