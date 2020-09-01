35fdc85cb74e0edfc706d6040d61a316915a3fa8
gugun_guntur <gugunguntur99@gmail.com>
2020-09-01 08:23:33 +0700

    add ganalityc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,16 @@
 
 
         <link href="{{ asset('quitelight/common/styles.css') }}" rel="stylesheet">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176918173-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-176918173-1');
+        </script>
+
 
     </head>
     <body>
