fa25b47394f18fe1c880ce7b223e86392f807b73
gugun_guntur <gugunguntur99@gmail.com>
2020-09-03 10:43:21 +0700

    set redis

--- config/database.php
+++ config/database.php
@@ -124,11 +124,12 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'parameters' => ['password' => env('REDIS_PASSWORD', null)],
         ],
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', 'redis'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
