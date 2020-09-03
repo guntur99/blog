d0bec19a85e8670f77c62594e4750363430a00c9
gugun_guntur <gugunguntur99@gmail.com>
2020-09-03 14:50:58 +0700

    fix redis

--- config/database.php
+++ config/database.php
@@ -130,7 +130,7 @@ return [
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', 'redis'),
-            'password' => env('REDIS_PASSWORD', 'galaxy*origami'),
+            'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
